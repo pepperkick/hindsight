9d70ff2343ca5a9a241816ae5fcfdd9d23c51791
PepperKick <abhishrant.m@gmail.com>
2021-05-24 19:04:38 +0530

    Make the service directly callable

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const service = app.select(AppModule).get(AppService);
+  await service.start();
+  await app.close();
+  process.exit(0);
 }
+
 bootstrap();
